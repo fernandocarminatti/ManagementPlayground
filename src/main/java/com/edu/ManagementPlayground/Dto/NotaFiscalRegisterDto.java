b92633815000aec7cb55c259fad400e4b5a72549
Fernando Carminatti <fernando.carminatti@gmail.com>
2025-06-04 15:41:20 -0300

    Add: GlobalExceptionHandler
    GlobalExceptionHandler for exceptions on bad requests or internal error.
    Currently not using @NotEmpty custom validation, throws error with some
    file types.

--- src/main/java/com/edu/ManagementPlayground/Dto/NotaFiscalRegisterDto.java
+++ src/main/java/com/edu/ManagementPlayground/Dto/NotaFiscalRegisterDto.java
@@ -2,7 +2,10 @@ package com.edu.ManagementPlayground.Dto;
 
 import com.edu.ManagementPlayground.Dto.CustomValidation.FileSize;
 import com.edu.ManagementPlayground.Dto.CustomValidation.FileType;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
@@ -22,7 +25,6 @@ public record NotaFiscalRegisterDto(
         @NotNull(message = "Supplier identification must be assigned.")
         Long supplierId,
 
-        @NotEmpty(message = "File must be provided.")
         @FileSize(max = 10 * 1024 * 1024, message = "File cannot exceed 10MB")
         @FileType(allowed = {"application/pdf", "application/xml"}, message = "Allowed formats are XML and PDF.")
         MultipartFile objectFile
---
+++ src/main/java/com/edu/ManagementPlayground/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.edu.ManagementPlayground.Exceptions;
+
+import jakarta.validation.UnexpectedTypeException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<Map<String, Object>> handleIllegalArgumentException(IllegalArgumentException e) {
+        Map<String, Object> errorsReturn = new HashMap<>();
+        List<String> errorList = new ArrayList<>();
+        errorList.add(e.getMessage());
+        errorsReturn.put("Reasons: ", errorList);
+        return ResponseEntity.status(400).body(errorsReturn);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, Object> errorsReturn = new HashMap<>();
+        List<String> errorList = new ArrayList<>();
+        e.getBindingResult().getAllErrors().forEach(error -> errorList.add(error.getDefaultMessage()));
+        errorsReturn.put("Reasons: ", errorList);
+        return ResponseEntity.status(400).body(errorsReturn);
+    }
+
+    @ExceptionHandler(UnexpectedTypeException.class)
+    private ResponseEntity<Map<String, String>> handleUnexpectedTypeException(UnexpectedTypeException e){
+        return ResponseEntity.status(500).body(Map.of("Error", "Internal server error."));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    private ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(400).body(Map.of("Error: ", "Invalid JSON Structure."));
+    }
+
+    @ExceptionHandler(StorageException.class)
+    protected ResponseEntity<Map<String, Object>> handleStorageException(StorageException e) {
+        Map<String, Object> errorsReturn = new HashMap<>();
+        List<String> errorList = new ArrayList<>();
+        errorList.add(e.getMessage());
+        errorsReturn.put("Reasons: ", errorList);
+        return ResponseEntity.status(400).body(errorsReturn);
+    }
+}
