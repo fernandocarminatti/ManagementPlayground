6dfc4950139254972ab077399780171c1371b08c
Fernando Carminatti <fernando.carminatti@gmail.com>
2025-06-09 13:06:17 -0300

    Refactor: Dto should not have mapping logic.
    DTO Should not have mapping logic, this should be an actual mapper class
    to handle a set or all dto mappings to/from entities.

--- src/main/java/com/edu/ManagementPlayground/Dto/NotaFiscalResponseDto.java
+++ src/main/java/com/edu/ManagementPlayground/Dto/NotaFiscalResponseDto.java
@@ -1,19 +1,7 @@
 package com.edu.ManagementPlayground.Dto;
 
-import com.edu.ManagementPlayground.Entity.NotaFiscal;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record NotaFiscalResponseDto(String numberIdentifier, LocalDate issueDate, BigDecimal totalValue, String fileReference, long supplierIdentifier) {
-
-    public static NotaFiscalResponseDto fromEntity(NotaFiscal notaFiscal){
-        return new NotaFiscalResponseDto(
-                notaFiscal.getNumberIdentifier(),
-                notaFiscal.getIssueDate(),
-                notaFiscal.getTotalValue(),
-                notaFiscal.getFileReference(),
-                notaFiscal.getSupplier().getId() // Keep track of DB hits on this PROXY reference.
-        );
-    }
 }
--- src/main/java/com/edu/ManagementPlayground/Dto/SupplierResponseDto.java
+++ src/main/java/com/edu/ManagementPlayground/Dto/SupplierResponseDto.java
@@ -1,15 +1,4 @@
 package com.edu.ManagementPlayground.Dto;
 
-import com.edu.ManagementPlayground.Entity.Supplier;
-
 public record SupplierResponseDto(String name, String cnpj, String email, String phoneNumber) {
-
-    public static SupplierResponseDto fromEntity(Supplier supplier){
-        return new SupplierResponseDto(
-                supplier.getName(),
-                supplier.getCnpj(),
-                supplier.getEmail(),
-                supplier.getPhoneNumber()
-        );
-    }
 }
